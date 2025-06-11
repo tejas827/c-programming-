a0aa98096667b11094612a663017c5868141dd7f
tejas827 <tejasraje827@gmail.com>
2025-06-11 07:32:42 +0530

    emp_sal_slip

--- emp_sal_slip.c
+++ emp_sal_slip.c
@@ -2,15 +2,32 @@
 int main()
 {
     float sal,da,hra,tax,bs;
-    printf("enter salary=");
+    int eid;
+    char ename[100];
+    FILE *fp;
+
+    fp=fopen("salaryslip.txt","w");
+
+    printf("enter employee details=");
+    printf("ID :");
+    scanf("%d",&eid);
+    printf("\nenter name:");
+    scanf("%s",&ename);
+    printf("\nenter salary=");
     scanf("%f",&bs);
-    printf("enter DA=");
+    printf("\nenter DA=");
     scanf("%f",&da);
-    printf("enter HRA=");
+    printf("\nenter HRA=");
     scanf("%f",&hra);
-    printf("enter tax =");
+    printf("\nenter tax =");
     scanf("%f",&tax);
     sal=bs+((bs*(hra+da-tax))/100);
-    printf("salary = %.2f",sal);
+    fprintf(fp,"\n\t--------------Employee salary slip-------------");
+    fprintf(fp,"\n\tID           : %d",eid);
+    fprintf(fp,"\n\tName         : %s",ename);
+    fprintf(fp,"\n\tDA (%%)      :%f",da);
+    fprintf(fp,"\n\tHRA (%%)     :%f",hra);
+    fprintf(fp,"\n\tTAX (%%)     :%f",tax);
+    fprintf(fp,"\n\ttotal salary :%.2f",sal);
     return 0;
 }
